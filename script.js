c2d170dfe1af61db3059c35dbc0f3686b6bee38d
Behzad <saeedibehzad@gmail.com>
2022-03-01 14:43:59 +0330

    add ease out by js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const myBtn = document.querySelector(".btn")
 const myCircle = document.querySelector(".circles-container")
 const myMove=()=>{
-    let rotateDegree = Math.ceil(Math.random()*(2000-1000)+1000)
+    let rotateDegree = Math.ceil(Math.random()*(2500-1500)+1500)
     let id = null
     let pos = 0;
+    let incrementer = .01;
     clearInterval(id)
     id = setInterval(rotation, 1);
     function rotation(){
-        console.log(rotateDegree);
-        if(pos==rotateDegree){
+        console.log(rotateDegree,pos);
+        incrementer +=.0035
+        if(pos>rotateDegree){
             clearInterval(id)
         }
         else{
-            pos++
+            pos+=(1/incrementer +.3)
             document.querySelector(".circles-container").style.transform= `rotate(${pos}deg)`
         }
     }
