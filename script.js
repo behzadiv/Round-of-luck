306fa417174d136344094d58e4299f42417aaa22
Behzad <saeedibehzad@gmail.com>
2022-03-02 14:23:45 +0330

    add animation to arrow

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myBtn = document.querySelector(".btn")
+const myArrow = document.querySelector(".arrow")
 const myCircle = document.querySelector(".circles-container")
 const myMove=()=>{
     let rotateDegree = Math.ceil(Math.random()*(2500-1500)+1500)
@@ -8,14 +9,17 @@ const myMove=()=>{
     clearInterval(id)
     id = setInterval(rotation, 1);
     function rotation(){
-        console.log(rotateDegree,pos);
         incrementer +=.0035
         if(pos>rotateDegree){
             clearInterval(id)
+            myArrow.classList.remove("arrowAnim")
+            myArrow.style.transform = null
         }
         else{
             pos+=(1/incrementer +.3)
-            document.querySelector(".circles-container").style.transform= `rotate(${pos}deg)`
+            myCircle.style.transform= `rotate(${pos}deg)`
+            myArrow.style.transform = `rotateY(${pos}deg)  `
+            myArrow.classList.add("arrowAnim")
         }
     }
 }
